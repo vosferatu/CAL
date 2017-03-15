7db3696ca6461a41ad73722dd18d5890bde9167a
bmsp2 <bmsp2@Tiago>
2017-03-15 15:13:11 +0000

    Interface with ToDo list

---
+++ CG1/src/Interface.cpp
@@ -0,0 +1,30 @@
+/*
+ * Interface.cpp
+ *
+ *  Created on: 15/03/2017
+ *      Author: bmsp2
+ */
+
+#include <iostream>
+using namespace std;
+
+void interface(){
+	cout << "	BIKE SHARING	\n" << endl;
+	cout << "Em que local se encontra?\n";
+}
+
+int main(){
+	//TODO: Listagem de locais possíveis?
+	//TODO: Carregar grafo
+	//TODO: Mostrar ponto de partilha mais
+	//próximo de onde se encontra, com lugar
+	//disponível para a devolução da bicicleta
+	//TODO: Mostrar ponto de partilha mais
+	//próximo de onde se encontra, com lugar
+	//disponível para a devolução da bicicleta
+	//TODO: Mostrar ponto de partilha mais
+	//barato de onde se encontra, com lugar
+	//disponível para a devolução da bicicleta
+}
+
+

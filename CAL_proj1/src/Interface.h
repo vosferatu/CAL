d0a9e9be4538277f0179c0047163e5726bffeaea
up201505439 <up201505439@B03AZ0606.fe.up.pt>
2017-03-30 13:23:50 +0100

    ok

--- CAL_proj1/src/Interface.h
+++ CAL_proj1/src/Interface.h
@@ -41,14 +41,10 @@ void searchForRent(size_t &origin_ind, vector<CPoint> &pontos, Graph<Node> &graf
  */
 void searchForReturn(size_t &origin_ind, vector<CPoint> &pontos, Graph<Node> &grafo);
 
-<<<<<<< HEAD
 /*
  * @brief get user info
  * @param origin_ind Index of the origin CPoint
  * @param pontos CPoints vector
  * @param grafo Graph for searching
  */
-void clientInit(size_t &origin_ind, vector<User*> &utils, vector<CPoint> &pontos);
-=======
 void clientInit(size_t &origin_ind, vector<User*> &utils, vector<CPoint> &pontos, User* current_user);
->>>>>>> branch 'master' of https://github.com/JMendes25/CAL.git

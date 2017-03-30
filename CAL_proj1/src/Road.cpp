1c3d33b008800a343b5bb1df8c32b13c652380dc
João Mendes <Joo Mendes@172.30.16.134>
2017-03-30 10:49:57 +0100

    merge

--- CAL_proj1/src/Road.cpp
+++ CAL_proj1/src/Road.cpp
@@ -27,12 +27,3 @@ const std::string& Road::getName() const {
 bool Road::isTwoWay() const {
 	return two_way;
 }
-
-Road* searchRoad(int id, std::vector<Road*> &estradas) {
-	for (size_t i = 0; i < estradas.size(); i++) {
-		if (estradas[i]->getId() == id)
-			return estradas[i];
-	}
-
-	return NULL;
-}

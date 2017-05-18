fc5fe8095be9485a262d4c14bb12d514a8ae9e84
up201505439 <up201505439@B03AV1315.salas.feupsig.fe.up.pt>
2017-05-18 16:07:57 +0100

    exact search

--- CAL_proj1/src/Interface.cpp
+++ CAL_proj1/src/Interface.cpp
@@ -16,22 +16,22 @@ void searchExact(vector<CPoint> &pontos) {
 	cout << "\nWhat street you want to search for?\n";
 	getline(cin, ans);
 
-	t0 = std::chrono::high_resolution_clock::now();
+	//t0 = std::chrono::high_resolution_clock::now();
 
 	vector<CPoint> result = indStringMatching(pontos, ans);
 
-	t1 = std::chrono::high_resolution_clock::now();
+	//t1 = std::chrono::high_resolution_clock::now();
 
-	cout << "RESULT: ";
-	cout << chrono::duration_cast < std::chrono::nanoseconds
-			> (t1 - t0).count() * 1e-6 << endl;
+	//cout << "RESULT: ";
+	//cout << chrono::duration_cast < std::chrono::nanoseconds> (t1 - t0).count() * 1e-6 << endl;
 
-	int i = 0;
-	for (i; i < result.size(); i++) {
-		cout << endl << i + 1 << result.at(i).getName();
+	cout << endl;
+	size_t i = 0;
+	for (; i < result.size(); i++) {
+		cout << i + 1 << " - " << result.at(i).getName() << endl;
 	}
 	if (i == 0)
-		cout << "Logar deconhecido!\n";
+		cout << "Unknown place!\n";
 
 	return;
 }
